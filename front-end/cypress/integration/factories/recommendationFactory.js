e49cdb6c7de546f58cabff9ed02442778cb12ec7
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-05-07 09:49:55 -0300

    Update code and improve front-end recommendation factory

--- front-end/cypress/integration/factories/recommendationFactory.js
+++ front-end/cypress/integration/factories/recommendationFactory.js
@@ -1,9 +1,18 @@
 import { faker } from "@faker-js/faker";
 
+const youtubeLinks = [
+  "https://www.youtube.com/watch?v=1S5xqe3zw8Y",
+  "https://www.youtube.com/watch?v=kyrCIiwe4kQ",
+  "https://www.youtube.com/watch?v=5_5oE0ijhKg",
+  "https://www.youtube.com/watch?v=SNM_1d3mmtI",
+];
+
+const n = youtubeLinks.length;
+
 export default function recommendationFactory() {
   const recommendation = {
     name: faker.lorem.words(3),
-    youtubeLink: "https://www.youtube.com/watch?v=1S5xqe3zw8Y",
+    youtubeLink: youtubeLinks[Math.floor(n * Math.random())],
   };
 
   return recommendation;
